0bfbbf47becf2fe57a9b299f3bc8dacc813109cb
Thomson2412 <thomasm.w.fink@gmail.com>
2020-04-09 19:48:40 +0200

    Okay safari

--- pi/www/js/audiomanager.js
+++ pi/www/js/audiomanager.js
@@ -1,30 +1,30 @@
+let sounds = {};
+
 class AudioManager{
 
-    sounds = {}; 
-
-    init(fileobjects){
-        for(var i = 0; i < fileobjects.length; i++){
-            var name = fileobjects[i].filename.split(".")[0];
+    init(fileObjects){
+        for(let i = 0; i < fileObjects.length; i++){
+            let name = fileObjects[i].filename.split(".")[0];
             this.sound = document.createElement("audio");
-            this.sound.src = "/sound/" + fileobjects[i].filename;
+            this.sound.src = "/sound/" + fileObjects[i].filename;
             this.sound.setAttribute("preload", "auto");
             this.sound.setAttribute("controls", "none");
             this.sound.style.display = "none";
-            this.sound.volume = fileobjects[i].volume;
-            this.sound.loop = fileobjects[i].loop;
+            this.sound.volume = fileObjects[i].volume;
+            this.sound.loop = fileObjects[i].loop;
             document.body.appendChild(this.sound);
-            fileobjects[i]["sound"] = this.sound;
-            this.sounds[name] = fileobjects[i];
+            fileObjects[i]["sound"] = this.sound;
+            sounds[name] = fileObjects[i];
         }
     }
 
     play(name){
         this.stop(name);
-        this.sounds[name].sound.play();
+        sounds[name].sound.play();
     }
 
     stop(name){
-        this.sounds[name].sound.pause();
-        this.sounds[name].sound.currentTime = 0;
+        sounds[name].sound.pause();
+        sounds[name].sound.currentTime = 0;
     }
 }
